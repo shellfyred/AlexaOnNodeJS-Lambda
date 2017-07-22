9cbf4c4e28c6960c3516ac822305fdefb0588cf0
Ryan Mead <rmead@rmhfranchise.com>
2017-07-22 14:25:51 -0400

    multifeat: restructuring, reorganizing and optimizing lambda code
     - ignore config.js files
     + add apikey to config.js
     + include config.js.sample
     - remove `js` dir
     + move `AlexaSkill` to main dir
     + move ServerInfo to config.js (and use that info for requests)
     - strip out unused packages
     + rename `Tivo` object to `App`
     - strip out unused intents
     + use `https` instead of `http` for requests
     + include apiKey in all requests

--- lamba_code/index.js
+++ lamba_code/index.js
@@ -3,83 +3,43 @@
  * App ID for the skill
  */
 
-var AlexaSkill = require('js/AlexaSkill');
-var express = require('express');
-var request = require('request');
-var http = require('http')
-var serverinfo = require("js/serverinfo");
+var AlexaSkill = require('./AlexaSkill');
+var https = require('https')
 var config = require('./config');
 
-var APP_ID = config.appid; //replace with "amzn1.echo-sdk-ams.app.[your-unique-value-here]";
 
-var Tivo = function () {
-    AlexaSkill.call(this, APP_ID);
+var App = function () {
+    AlexaSkill.call(this, config.Appid);
 };
 
 // Extend AlexaSkill
-Tivo.prototype = Object.create(AlexaSkill.prototype);
-Tivo.prototype.constructor = Tivo;
+App.prototype = Object.create(AlexaSkill.prototype);
+App.prototype.constructor = App;
 
-Tivo.prototype.eventHandlers.onSessionStarted = function (sessionStartedRequest, session) {
-    console.log("Tivo onSessionStarted requestId: " + sessionStartedRequest.requestId
+App.prototype.eventHandlers.onSessionStarted = function (sessionStartedRequest, session) {
+    console.log("App onSessionStarted requestId: " + sessionStartedRequest.requestId
         + ", sessionId: " + session.sessionId);
     // any initialization logic goes here
 };
 
-Tivo.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
-    console.log("Tivo onLaunch requestId: " + launchRequest.requestId + ", sessionId: " + session.sessionId);
+App.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
+    console.log("App onLaunch requestId: " + launchRequest.requestId + ", sessionId: " + session.sessionId);
     var speechOutput = "Welcome to the TV Controls, I can control the TV for you.";
     var repromptText = "You can say change channel to with the name of the channel.";
     response.ask(speechOutput, repromptText);
 };
 
-Tivo.prototype.eventHandlers.onSessionEnded = function (sessionEndedRequest, session) {
-    console.log("Tivo onSessionEnded requestId: " + sessionEndedRequest.requestId
+App.prototype.eventHandlers.onSessionEnded = function (sessionEndedRequest, session) {
+    console.log("App onSessionEnded requestId: " + sessionEndedRequest.requestId
         + ", sessionId: " + session.sessionId);
     // any cleanup logic goes here
 };
 
-Tivo.prototype.intentHandlers = {
+App.prototype.intentHandlers = {
     // register custom intent handlers
 
     HelpIntent: function (intent, session, response) {
         response.ask("You can tell the TV to turn off and on, and request for various channels.");
-    },
-
-    newEpisodeIntent: function (intent, session, response) {
-        var name = intent.slots.titlename.value; //pulls variable from intent
-        var header = {'showname' : name};
-
-        sendCommand("/plex/playlatestepisode",header,null,function ResponseCallback(res) {
-            console.log(res);
-            response.tell(res);
-        });           
-    },
-
-    searchMovieIntent: function (intent, session, response) {
-        var name = intent.slots.titlename.value; //pulls variable from intent
-        var header = {'moviename' : name};
-        console.log("received request to play " + name);
-
-        sendCommand("/plex/searchmovie",header,null,function ResponseCallback(res) {
-            console.log(res);
-            response.tell(res);
-        });           
-    },
-
-    recentlyAddedIntent: function (intent, session, response) {
-        var mediatype = intent.slots.mediatype.value; //pulls variable from intent
-        
-        if (mediatype == "shows") {
-            mediatype="episodes";
-        }
-        var header = {'mediatype' : mediatype};
-        console.log("received request to play " + mediatype);
-
-        sendCommand("/plex/recentlyadded",header,null,function ResponseCallback(res) {
-            console.log(res);
-            response.tell(res);
-        });           
     },
 
     TVPowerIntent: function (intent, session, response) {
@@ -87,7 +47,7 @@ Tivo.prototype.intentHandlers = {
 
         sendCommand("/sonybravia/power",header,null,function ResponseCallback(res) {
             console.log(res);
-            response.tell("Initial TV Power " + powerintent + " sent from Lambda to home server.");
+            response.tell(res);
         });           
     },
 
@@ -99,70 +59,20 @@ Tivo.prototype.intentHandlers = {
             console.log(res);
             response.tell(res);
         });      
-    },
-
-    EverythingPowerIntent: function (intent, session, response) {
-        //sets the users power intent variable
-        var powerintent = intent.slots.power.value;
-        var header = {'powerintent': powerintent};
-        var speechOutput = "";
-
-        console.log("User has requested that the TV turn " + powerintent);
-
-        sendCommand("/sonybravia/power",header,null,function ResponseCallback(res) {
-            console.log(res);
-            speechOutput += res + ", ";
-
-            //determine if TV has actually been switched off/on
-            if(res == "I have switched the TV "+powerintent) {
-                sendCommand("/tivo/power",header,null,function ResponseCallback(res) {
-                    console.log(res);
-                    speechOutput += res;
-
-                    response.tell(speechOutput);
-                });  
-            } else {
-                response.tell(speechOutput);
-            }
-        });   
-       
-    },
-
-    ChannelIntent: function (intent, session, response) {
-        //Match name of channel to the corresponding number in channel-list.
-        var channelname = intent.slots.channel.value.toLowerCase();   
-        var header = {'channelname': channelname};
-
-        sendCommand("/tivo/changechannel",header,null,function ResponseCallback(res) {
-            console.log(res);
-            response.tell(res);
-        }); 
-    },
-
-    ClosedCapIntent: function (intent, session, response) {
-        //Match name of channel to the corresponding number in channel-list.
-        var statusintent = intent.slots.cc.value.toLowerCase();   
-        console.log("Closed caption has been requested " + statusintent + " by user.");
-        var header = {'cc': statusintent};
-
-        sendCommand("/tivo/cc",header,null,function ResponseCallback(res) {
-            console.log(res);
-            response.tell(res);
-        }); 
-    },
-
+    }
 };
 
 function sendCommand(path,header,body,callback) {
+    header.apiKey = config.apiKey;
     var opt = {
-        host:serverinfo.host,
-        port:serverinfo.port,
+        host:config.host,
+        port:config.port,
         path: path,
         method: 'POST',
         headers: header,
     };
 
-    var req = http.request(opt, function(res) {
+    var req = https.request(opt, function(res) {
         res.setEncoding('utf8');
         res.on('data', function (chunk) {
             console.log('Response: ' + chunk);
@@ -177,8 +87,8 @@ function sendCommand(path,header,body,callback) {
 
 // Create the handler that responds to the Alexa Request.
 exports.handler = function (event, context) {
-    // Create an instance of the Tivo skill.
-    var tivo = new Tivo();
-    tivo.execute(event, context);
+    // Create an instance of the App skill.
+    var app = new App();
+    app.execute(event, context);
 };
 
--- lamba_code/js/serverinfo.js
+++
@@ -1,2 +0,0 @@
-exports.host = "<external IP of the node server> ";
-exports.port = "port of the external IP you natted on the firewall";
